4d44556d84961f5fbd36def14482f006fd5aa88c
IkhsanHadi <ikhsanhdn747@gmail.com>
2025-08-26 11:36:08 +0700

    map

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,11 +1,18 @@
 let map;
+let colors = [
+    "#3cb44b","#e6194b","#ffe119","#4363d8","#f58231","#911eb4",
+    "#46f0f0","#f032e6","#bcf60c","#fabebe","#008080","#e6beff",
+    "#9a6324","#fffac8","#800000","#aaffc3","#808000","#ffd8b1",
+    "#000075",
+];
+
 function initMap() {
     // const jakarta = { lat: 0.5, lng: 110 };
-    const jakarta = { lat: -3.695, lng: 128.181 };
+    const jakarta = { lat: -5.595, lng: 129.181 };
 
     // Inisialisasi peta
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 8,
         center: jakarta,
         gestureHandling: 'greedy', // Mengizinkan zoom menggunakan scroll
         mapTypeId: google.maps.MapTypeId.HYBRID,
@@ -65,6 +72,95 @@ function initMap() {
             infowindow.open(map);
         }
     });
+
+    const wsAmbonSeram = '/data/WS_AMBON-SERAM.json';
+    const wsYamdena = '/data/WS_KEPULAUAN_YAMDENA-WETAR.json';
+    const dasAmbonSeram = '/data/DAS AMBON-SERAM.json';
+    const dasYamdena = '/data/DAS KEPULAUAN YAMDENA-WETAR.json';
+    const sungaiMaluku = "/data/Sungai_WS_AMBON-SERAM.json";
+    const sungaiYamdena = "/data/Sungai_WS_KEPULAUAN_YAMDENA-WETAR.json";
+
+    loadWsJson(wsAmbonSeram, '#e6194b');
+    loadWsJson(wsYamdena, '#ffe119');
+    loadDasJson(dasAmbonSeram, '#911eb4');
+    loadDasJson(dasYamdena, '#46f0f0');
+    // loadRiverJson(sungaiYamdena);
+    // loadRiverJson(sungaiMaluku);
+}
+
+function loadWsJson(url, color) {
+    fetch(url)
+        .then(res => res.json())
+        .then(topoData => {
+            const objectName = Object.keys(topoData.objects)[0];
+            const geojson = topojson.feature(topoData, topoData.objects[objectName]);
+
+            // Buat Data Layer baru
+            const dataLayer = new google.maps.Data({ map: map });
+
+            // Tambahkan GeoJSON ke layer ini
+            dataLayer.addGeoJson(geojson);
+
+            // Styling khusus untuk layer ini
+            dataLayer.setStyle({
+                fillColor: color,
+                fillOpacity: 0.4,
+                strokeColor: "black",
+                strokeWeight: 1,
+                zIndex: 1
+            });
+        })
+        .catch(err => console.error("Error load topojson:", err));
+}
+
+function loadDasJson(url, color) {
+    fetch(url)
+        .then(res => res.json())
+        .then(topoData => {
+            const objectName = Object.keys(topoData.objects)[0];
+            const geojson = topojson.feature(topoData, topoData.objects[objectName]);
+
+            // Buat Data Layer baru
+            const dataLayer = new google.maps.Data({ map: map });
+
+            // Tambahkan GeoJSON ke layer ini
+            dataLayer.addGeoJson(geojson);
+
+            // Styling khusus untuk layer ini
+            dataLayer.setStyle({
+                fillColor: color,
+                fillOpacity: 0.4,
+                strokeColor: "black",
+                strokeWeight: 1,
+                zindex: 2
+            });
+        })
+        .catch(err => console.error("Error load topojson:", err));
+}
+
+function loadRiverJson(url, color = "#0000ff") {
+    fetch(url)
+        .then(res => res.json())
+        .then(topoData => {
+            const objectName = Object.keys(topoData.objects)[0];
+            const geojson = topojson.feature(topoData, topoData.objects[objectName]);
+
+            // Buat Data Layer baru untuk sungai
+            const riverLayer = new google.maps.Data({ map: map });
+
+            // Tambahkan GeoJSON ke layer sungai
+            riverLayer.addGeoJson(geojson);
+
+            // Styling garis sungai
+            riverLayer.setStyle({
+                strokeColor: color,
+                strokeOpacity: 0.4,
+                strokeWeight: 1,
+                fillOpacity: 0, // sungai biasanya bukan polygon
+                zIndex: 3
+            });
+        })
+        .catch(err => console.error("Error load river topojson:", err));
 }
 
 $(document).ready(function() {
